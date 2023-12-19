390b742f558df37f5a190a139d84e63f01c8b826
turichetoya@gmail.com <turichetoya@gmail.com>
2023-12-19 23:03:54 +0400

    Added npc

---
+++ Assets/conversation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using DialogueEditor;
+
+
+public class conversation : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConversation;
+
+
+    private void OnTriggerStay(Collider other)  
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
+        }
+
+    }
+}
